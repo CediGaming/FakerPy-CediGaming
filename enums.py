be2893427067a6c4c854cc3f651f8ede91de27cf
CediGaming <capsland2709@gmail.com>
2022-02-05 11:52:19 +0100

    Refactoring

--- enums.py
+++ enums.py
@@ -1,10 +1,10 @@
 from enum import Enum
 
 class CountryCode(Enum):
-	DE = "de"
-	US = "us"
+    DE = "de"
+    US = "us"
 
 class Gender(Enum):
-	MALE = "m"
-	FEMALE = "f"
-	RANDOM = "r"
+    MALE = "m"
+    FEMALE = "f"
+    RANDOM = "r"
--- person.py
+++ person.py
@@ -2,56 +2,69 @@ import json
 from random import randint
 from . enums import CountryCode, Gender
 
-_data_folder = __file__ + "/../data/"
+_DATA_FOLDER = __file__ + "/../data/"
 
-_mail_domains = json.load(open(_data_folder + "mail_domains.json"))
-_passchars = list("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789*_.,!?&%$=()[]}{\/")
+with open(_DATA_FOLDER + "mail_domains.json", encoding="UTF-8") as file:
+    _mail_domains = json.load(file)
+
+_passchars = list(r"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijkl" +
+                  r"mnopqrstuvwxyz0123456789*_.,!?&%$=()[]}{\/")
 
 class Person:
-	def __init__(self, firstname, lastname, mailaddress, password, street, hsnr, plz, city):
-		self.firstname = firstname
-		self.lastname = lastname
-		self.mailaddress = mailaddress
-		self.password = password
-		self.street = street
-		self.hsnr = hsnr
-		self.plz = plz
-		self.city = city
-	
-	def __str__(self):
-		return self.getFullName()
+    def __init__(self, firstname, lastname, mailaddress, password, street, hsnr, plz, city):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.mailaddress = mailaddress
+        self.password = password
+        self.street = street
+        self.hsnr = hsnr
+        self.plz = plz
+        self.city = city
 
-	def getFullName(self):
-		return self.firstname + " " + self.lastname
+    def __str__(self):
+        return self.get_full_name()
 
-	def getFullAddress(self):
-		return self.street + " " + self.hsnr + ", " + self.plz + " " + self.city
+    def get_full_name(self):
+        return self.firstname + " " + self.lastname
+
+    def get_full_address(self):
+        return self.street + " " + self.hsnr + ", " + self.plz + " " + self.city
 
 
-def randomPerson(countrycode: CountryCode = CountryCode.US, gender: Gender = Gender.RANDOM):
-	if gender == Gender.RANDOM:
-		_first_names_m = json.load(open(_data_folder + "first_names_" + countrycode.value + "_m.json", encoding="UTF-8"))
-		_first_names_f = json.load(open(_data_folder + "first_names_" + countrycode.value + "_f.json", encoding="UTF-8"))
-		_first_names = _first_names_f + _first_names_m
-	elif gender == Gender.MALE:
-		_first_names = json.load(open(_data_folder + "first_names_" + countrycode.value + "_m.json", encoding="UTF-8"))
-	elif gender == Gender.FEMALE:
-		_first_names = json.load(open(_data_folder + "first_names_" + countrycode.value + "_f.json", encoding="UTF-8"))
-	
-	_last_names = json.load(open(_data_folder + "last_names_" + countrycode.value + ".json", encoding="UTF-8"))
-	_street_names = json.load(open(_data_folder + "street_names_" + countrycode.value + ".json", encoding="UTF-8"))
-	_city_names = json.load(open(_data_folder + "city_names_" + countrycode.value + ".json", encoding="UTF-8"))
+def random_person(countrycode: CountryCode = CountryCode.US, gender: Gender = Gender.RANDOM):
+    if gender == Gender.RANDOM:
+        with open(_DATA_FOLDER + "first_names_" + countrycode.value + "_m.json", encoding="UTF-8") as file:
+            _first_names_m = json.load(file)
+        with open(_DATA_FOLDER + "first_names_" + countrycode.value + "_f.json", encoding="UTF-8") as file:
+            _first_names_f = json.load(file)
 
-	firstname = _first_names[randint(0, len(_first_names) - 1)]
-	lastname = _last_names[randint(0, len(_last_names) - 1)]
-	maildomain = _mail_domains[randint(0, len(_mail_domains) - 1)]
-	password = ""
-	street = _street_names[randint(0, len(_street_names) - 1)]
-	hsnr = str(randint(1,512))
-	plz = str(randint(10000, 99999))
-	city = _city_names[randint(0, len(_city_names) - 1)]
+        _first_names = _first_names_f + _first_names_m
 
-	for p in range(randint(5,12)):
-		password += _passchars[randint(0,len(_passchars) - 1)]
-	
-	return Person(firstname, lastname, firstname+"."+lastname+"@"+maildomain, password, street, hsnr, plz, city)
+    else:
+        with open(_DATA_FOLDER + "first_names_" + countrycode.value + "_" + gender.value + ".json", encoding="UTF-8") as file:
+            _first_names = json.load(file)
+
+
+    _last_names = json.load(open(
+        _DATA_FOLDER + "last_names_" + countrycode.value + ".json", encoding="UTF-8"))
+
+    _street_names = json.load(open(
+        _DATA_FOLDER + "street_names_" + countrycode.value + ".json", encoding="UTF-8"))
+
+    _city_names = json.load(open(
+        _DATA_FOLDER + "city_names_" + countrycode.value + ".json", encoding="UTF-8"))
+
+    firstname = _first_names[randint(0, len(_first_names) - 1)]
+    lastname = _last_names[randint(0, len(_last_names) - 1)]
+    maildomain = _mail_domains[randint(0, len(_mail_domains) - 1)]
+    mailaddress = firstname + "." + lastname + "@" + maildomain
+    password = ""
+    street = _street_names[randint(0, len(_street_names) - 1)]
+    hsnr = str(randint(1,512))
+    plz = str(randint(10000, 99999))
+    city = _city_names[randint(0, len(_city_names) - 1)]
+
+    for _ in range(randint(5,12)):
+        password += _passchars[randint(0,len(_passchars) - 1)]
+
+    return Person(firstname, lastname, mailaddress, password, street, hsnr, plz, city)
